676ab3e0d53922103663254299df8a170dd96e2b
Nathan Desjardins <Nathan.desjardins@baxter-academy.org>
2019-01-24 12:26:05 -0500

    Better player movement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int speed = 2;
+    public int WeaponHolding;
+    public string projectileName;
     void Start()
     {
         
     }
-
+    public virtual void Shoot()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        GameObject projectile = GameObject.Instantiate((GameObject)Resources.Load(projectileName));
+        projectile.transform.rotation = transform.LookAt(mousePosition, Vector3.up);
+    }
     void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(2, 0, 0), Time.deltaTime * speed);
-        } else if (Input.GetKey(KeyCode.A))
+        } if (Input.GetKey(KeyCode.A))
         {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(-2, 0, 0), Time.deltaTime * speed);
-        } else if (Input.GetKey(KeyCode.W))
+        } if (Input.GetKey(KeyCode.W))
         {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, 2, 0), Time.deltaTime * speed);
         }
-        else if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, -2, 0), Time.deltaTime * speed);
         }
---
+++ Assets/ProjectileBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour
+{
+    public int damage;
+    public virtual void OnHit()
+    {
+        Destroy(gameObject);
+    }
+}
